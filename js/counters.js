c3f0ccb2940b4e1f0fb871463906cba50cdc2f33
Liam <ltaylor.ulysses@gmail.com>
2023-02-07 17:22:29 -0500

    remote push

---
+++ js/counters.js
@@ -0,0 +1,7 @@
+document.getElementById("clicks").innerHTML = clicks;
+
+$('.like-counter').click(function() {
+  clicks += 1;
+  document.getElementById("clicks").innerHTML = clicks;
+  $('.like-counter').addClass("liked");
+});
