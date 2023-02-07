f4008e10aa9c7b328aa754716a66a10c5a640011
ltaylor2 <ltaylor.ulysses@gmail.com>
2023-02-07 18:25:15 -0500

    Now reads json-format list (currently embedded in index.html) plus DYNAMIC and REACTIVE votecounters

--- js/counters.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("clicks").innerHTML = clicks;
-
-$('.like-counter').click(function() {
-  clicks += 1;
-  document.getElementById("clicks").innerHTML = clicks;
-  $('.like-counter').addClass("liked");
-});
